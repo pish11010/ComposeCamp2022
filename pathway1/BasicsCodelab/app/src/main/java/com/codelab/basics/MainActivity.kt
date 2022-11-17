25de77c200f5a0795f0d07870d0a5c693f8198cf
Jake Choi <jake@delightroom.com>
2022-11-17 15:16:10 +0900

    9. ~ 11.

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -3,11 +3,16 @@ package com.codelab.basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -51,7 +56,7 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun OnboardingScreen(
   onContinueClicked: () -> Unit,
-  modifier: Modifier = Modifier
+  modifier: Modifier = Modifier,
 ) {
   Column(
     modifier = modifier.fillMaxSize(),
@@ -71,10 +76,10 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
   modifier: Modifier = Modifier,
-  names: List<String> = listOf("World", "Compose")
+  names: List<String> = List(1000) { "$it" },
 ) {
-  Column(modifier = modifier.padding(vertical = 4.dp)) {
-    for (name in names) {
+  LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+    items(items = names) { name ->
       Greeting(name = name)
     }
   }
@@ -90,9 +95,16 @@ fun OnboardingPreview() {
 
 @Composable
 private fun Greeting(name: String) {
-  val expanded = remember { mutableStateOf(false) }
-  val extraPadding = if (expanded.value) 48.dp else 0.dp
 
+  var expanded by remember { mutableStateOf(false) }
+
+  val extraPadding by animateDpAsState(
+    if (expanded) 48.dp else 0.dp,
+    animationSpec = spring(
+      dampingRatio = Spring.DampingRatioMediumBouncy,
+      stiffness = Spring.StiffnessLow
+    )
+  )
   Surface(
     color = MaterialTheme.colors.primary,
     modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -100,15 +112,15 @@ private fun Greeting(name: String) {
     Row(modifier = Modifier.padding(24.dp)) {
       Column(modifier = Modifier
         .weight(1f)
-        .padding(bottom = extraPadding)
+        .padding(bottom = extraPadding.coerceAtLeast(0.dp))
       ) {
         Text(text = "Hello, ")
         Text(text = name)
       }
       Button(
-        onClick = { expanded.value = !expanded.value }
+        onClick = { expanded = !expanded }
       ) {
-        Text(if (expanded.value) "Show less" else "Show more")
+        Text(if (expanded) "Show less" else "Show more")
       }
     }
   }
